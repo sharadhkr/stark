258d0ff64c52482e2e85bd8013b74941a7f2e709
sharadhkr <www.sharadrathoreji@gmail.com>
2025-04-14 20:41:25 +0530

    Ds

--- client/src/Components/bottom.jsx
+++ client/src/Components/bottom.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-const Bottom = () => {
-  <div className="w-full bg-gradient-to-br from-blue-200 to-blue-200  pb-25">
-    <hr className="border-t  border-gray-600" />
-    <div className=" text-center text-sm text-gray-700 py-4">
-      &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+
+function Bottom() {
+  return (
+    <div className="w-full bg-gradient-to-br from-blue-200 to-blue-200  pb-25">
+      <hr className="border-t  border-gray-600" />
+      <div className=" text-center text-sm text-gray-700 py-4">
+        &copy; {new Date().getFullYear()} Your Company Name. All rights reserved.
+      </div>
     </div>
-  </div>
-};
+  );
+}
 
 export default Bottom;
