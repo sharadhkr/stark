2803ec3f6ba3a22c1f4792d6f48d08be81e130f2
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-03-12 23:26:35 +0530

    forntend

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,30 @@
+// ✅ src/Layout.jsx
+import React, { useMemo } from 'react';
+import { useLocation, matchPath } from 'react-router-dom';
+import BottomNavbar from './Components/Navbar';
+import Bottom from './Components/botttommm';
+
+const userRoutes = [
+  '/', '/cart', '/wishlist', '/dashboard', '/category/:categoryName',
+  '/seller/:sellerId', '/product/:productId', '/seller/products',
+  '/seller/orders', '/order/:orderId', '/search', '/combo/:comboId',
+];
+
+export const Navbar = React.memo(({ children }) => {
+  const { pathname } = useLocation();
+
+  const showNavbar = useMemo(() => {
+    return userRoutes.some((path) => matchPath({ path, end: path === '/' }, pathname));
+  }, [pathname]);
+
+  return (
+    <>
+      {children}
+      {showNavbar && (
+        <>
+          <BottomNavbar />
+        </>
+      )}
+    </>
+  );
+});
