ca4abf6b028b939155afc453101446d4ebe2e1b7
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-03-15 23:34:55 +0530

    won

--- server/server.js
+++ server/server.js
@@ -6,4 +6,84 @@ const server = http.createServer(app);
 
 server.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000
+const server = http.createServer(app);
+
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
 })
